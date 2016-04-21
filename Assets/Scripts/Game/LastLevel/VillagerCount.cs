806bf24c935b0464b0f84b63cf159673bbc0e562
LJOORHDN <jmm7115@rit.edu>
2016-04-21 15:29:30 -0400

    Worked on last level a bit

---
+++ Assets/Scripts/Game/LastLevel/VillagerCount.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class VillagerCount : MonoBehaviour {
+    public int villagerCount;
+    private int lastCount;
+    public Text countText;
+
+	// Use this for initialization
+	void Start () {
+        villagerCount = GameObject.FindGameObjectsWithTag("Villager").Length;
+        lastCount = villagerCount; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(lastCount != villagerCount)
+        {
+            
+            countText.text = villagerCount.ToString();
+            lastCount = villagerCount;
+            
+            
+        }
+	}
+}
