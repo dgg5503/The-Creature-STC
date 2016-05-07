77b9453287979af9f96065176b9357dfb01943eb
Alex <aas9922@rit.edu>
2016-05-06 22:00:45 -0400

    Working on flying camera

---
+++ Assets/Scripts/Game/WayPoint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WayPoint : MonoBehaviour {
+
+    public List<GameObject> wayPoints = new List<GameObject>();
+
+    void Awake() {
+    }
+    
+    // Use this for initialization
+	void Start () {
+      
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (GameObject wayPointToGo in wayPoints)
+        {
+            
+            moveCamera(wayPointToGo.GetComponent<Transform>().position);
+        }
+
+
+	}
+
+    public void moveCamera(Vector3 moveToPosition) {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, moveToPosition, 2 * Time.deltaTime); 
+    }
+}
